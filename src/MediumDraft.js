544d5a96bd94cf2a531ca40e332dfb69820f3fa5
ruucm <ruucm.a@gmail.com>
2018-06-04 08:22:26 +0900

    Init

---
+++ src/MediumDraft.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// if using webpack
+// import 'medium-draft/lib/index.css';
+
+import { Editor, createEditorState } from './components/medium-draft'
+
+class DemoApp extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editorState: createEditorState(), // for empty content
+    }
+
+    /*
+    this.state = {
+      editorState: createEditorState(data), // with content
+    };
+    */
+
+    this.onChange = editorState => {
+      this.setState({ editorState })
+    }
+  }
+
+  componentDidMount() {
+    this.refs.editor.focus()
+  }
+
+  render() {
+    const { editorState } = this.state
+    return (
+      <Editor ref="editor" editorState={editorState} onChange={this.onChange} />
+    )
+  }
+}
+
+export default DemoApp
